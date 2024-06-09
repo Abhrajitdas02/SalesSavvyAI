73f0352b291ae70e98f872ca47192907f3e3b565
Abhrajit Das <abhrajitdas12245@gmail.com>
2024-06-09 16:10:38 +0530

    updated env

--- my-app/src/middleware.ts
+++ my-app/src/middleware.ts
@@ -1,10 +1,10 @@
-import { authMiddleware} from "@clerk/nextjs/server";
+import { authMiddleware } from '@clerk/nextjs'
 
 export default authMiddleware({
-   publicRoutes: ["/","/auth(.*)","/portal(.*)","/images(,*)"],
-   ignoredRoutes: ["/chatbot"],
+  publicRoutes: ['/', '/auth(.*)', '/portal(.*)', '/images(.*)'],
+  ignoredRoutes: ['/chatbot'],
 })
 
-export const config={
-    matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+export const config = {
+  matcher: ['/((?!.+.[w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 }
