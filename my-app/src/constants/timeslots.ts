8eed6af11e0277e453d7f053a1eba29c692b433c
Abhrajit Das <abhrajitdas12245@gmail.com>
2024-06-26 03:12:55 +0530

    updated backend

--- my-app/src/constants/timeslots.ts
+++ my-app/src/constants/timeslots.ts
@@ -6,39 +6,6 @@ type AppointmentTimeSlots = {
 
 export const APPOINTMENT_TIME_SLOTS: AppointmentTimeSlots[] = [
     {
-        slot: '9:00am',
-    },
-    {
-        slot: '9:30am',
-    },
-    {
-        slot: '10:00am',
-    },
-    {
-        slot: '10:30am',
-    },
-    {
-        slot: '11:00am',
-    },
-    {
-        slot: '11:30am',
-    },
-    {
-        slot: '12:00pm',
-    },
-    {
-        slot: '12:30pm',
-    },
-    {
-        slot: '1:00pm',
-    },
-    {
-        slot: '1:30pm',
-    },
-    {
-        slot: '2:30pm',
-    },
-    {
       slot: '3:30pm',
     },
     {
@@ -55,21 +22,6 @@ export const APPOINTMENT_TIME_SLOTS: AppointmentTimeSlots[] = [
     },
     {
       slot: '6:00pm',
-    },
-    {
-        slot: '6:30pm',
-    },
-    {
-        slot: '7:00pm',
-    },
-    {
-        slot: '7:30pm',
-    },
-    {
-        slot: '8:00pm',
-    },
-    {
-        slot: '8:30pm',
-    },
+    }
   ]
   
